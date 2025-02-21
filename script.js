536b14a9afaac081d2ad243c9ee3eb6fab83e756
Arya Saputra <aryasaputra3046@gmial.com>
2025-02-21 17:50:18 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Assign
+const alert = document.querySelector('.alert')
+const form = document.querySelector('.textbox')
+const grocery = document.querySelector('.itemInput')
+const submitBtn = document.querySelector('.submit-btn')
+const list = document.querySelector('.itemList')
+const container = document.querySelector('.itemsContain')
