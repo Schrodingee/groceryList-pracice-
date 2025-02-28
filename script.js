19b42015955e86207e12db2da14cae4d75acc664
Arya Saputra <aryasaputra3046@gmial.com>
2025-02-28 10:19:01 +0800

    done assigning dom in js and making addList function

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-// Assign
+// Assign query
 const alert = document.querySelector('.alert')
 const form = document.querySelector('.textbox')
-const grocery = document.querySelector('.itemInput')
+const grocery = document.getElementById('itemInput')
 const submitBtn = document.querySelector('.submit-btn')
 const list = document.querySelector('.itemList')
 const container = document.querySelector('.itemsContain')
+const clearBtn = document.querySelector('.clear-btn')
+
+// edit var
+let editElement;
+let editFlag = false;
+let editID = '';
+
+//  Event Listeners
+// submit form
+form.addEventListener('submit',addItem)
+
+// Functions
+function addItem(e){
+    e.preventDefault();
+    console.log(grocery.value);
+}
